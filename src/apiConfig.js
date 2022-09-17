8b4948a1b04735ea85fb70f93088ec8b426828cd
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2022-09-16 21:53:13 -0400

    add deployed api url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://harmonyhaven.herokuapp.com',
+	production: 'https://ascchoirapi.herokuapp.com/',
 	development: 'http://localhost:8000',
 }
 
