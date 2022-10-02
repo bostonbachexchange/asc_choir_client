8e37a4f324498f266565ba0db621c2d7f955070a
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2022-10-02 15:19:21 -0400

    .

--- src/components/UpcomingMusic.js
+++ src/components/UpcomingMusic.js
@@ -12,7 +12,7 @@ const UpcomingMusic = () => {
 		<>
 		<div className='text-center'>
 			<h2>Upcoming Music</h2>
-			<h5><strong>October 2, 2022</strong></h5>
+			<h5><strong>October 9, 2022</strong></h5>
 			<div style={musicBorder}><strong>Prelude </strong>
 			<br></br>
 			<>piano</></div>
@@ -21,16 +21,16 @@ const UpcomingMusic = () => {
 			<Link to="songs/63379bfad6d1a05912c1355f">#11 Chalice Song</Link></div>
 			<div style={musicBorder}><strong>Opening Hymn </strong> 
 			<br></br>
-			<Link to="songs/63379bfad6d1a05912c1355f">336 All My Memories of Love</Link></div>
+			<Link to="songs/">TBD</Link></div>
 			<div style={musicBorder}><strong>Centering Music </strong> 
 			<br></br>
-			<Link to="songs/63379bfad6d1a05912c1355d">1037 We Begin Again in Love</Link></div>
+			<Link to="songs/"> TBD</Link></div>
 			<div style={musicBorder}><strong>Offertory </strong> 
 			<br></br>
-			<Link to='songs/63379bfad6d1a05912c13562'>Make Me a Channel of Your Peace (Piano or sung if volunteer)</Link></div>
+			<Link to='songs/'>TBD</Link></div>
 			<div style={musicBorder}><strong>Closing Hymn </strong> 
 			<br></br>
-			<Link to="songs/63379bfad6d1a05912c13560">Let There Be Peace On Earth</Link></div>
+			<Link to="songs/">TBD</Link></div>
 			<div style={musicBorder}><strong>Postlude </strong>
 			<br></br>
 			<>piano</></div>			<hr></hr>
