bcf9b7f9b104b7d8b9d29c71c065666f2bac9130
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2022-11-16 13:01:36 -0500

    .

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,7 +9,7 @@ const AboutUs = () => {
         <>
             <Card style={aboutUsStyle}>
                 <Card.Header className="text-center p-6" style={{fontSize: '32px'}}><strong>Our Choir</strong></Card.Header>
-                <Card.Body className="m-4">We are a small, but mighty, volunteer chorus. No matter what your skill level or your experience, you are welcome to sing with us. The best way to find our rehearsal schedule is to sign up for an account on our site, or contact our music director.</Card.Body>
+                <Card.Body className="m-4">We are a small, but mighty, volunteer chorus. No matter what your skill level or your experience, you are welcome to sing with us. The best way to find our rehearsal schedule is to sign up for an account on our site or contact our music director.</Card.Body>
                 <Card.Header><h2>Music Director</h2></Card.Header>
                 <Card.Body style={{height: 'auto'}}>
                     <p>Jacob Clapper</p>
--- src/components/UpcomingMusic.js
+++ src/components/UpcomingMusic.js
@@ -14,8 +14,8 @@ const UpcomingMusic = () => {
 	return (
 		<>
 		<div className='text-center'>
-			<h2 style={{fontSize: 40}}>Upcoming Music <br></br><em>Indigenous People's History Service</em></h2>
-			<h5 className='pt-2'><strong>Novemember 13, 2022</strong></h5>
+			<h2 style={{fontSize: 40}}>Upcoming Music <br></br><em>Guest Minister Florence Caplow</em></h2>
+			<h5 className='pt-2'><strong>Novemember 20, 2022</strong></h5>
 			<div style={musicBorder} ><strong>Prelude </strong>
 			<br></br>
 			<>piano</></div>
@@ -24,16 +24,41 @@ const UpcomingMusic = () => {
 			<Link to="songs/" className='pt-2'>#1013 Open My Heart</Link></div>
 			<div style={musicBorder}><strong>Opening Hymn </strong> 
 			<br></br>
-			<Link to="songs/635a03326de76c4c72db162a"> #360 Here We Have Gathered</Link></div>
+			<Link to="songs/635a03326de76c4c72db162a"> #368 Gathered Here</Link></div>
 			<div style={musicBorder}><strong>Centering Hymn </strong> 
 			<br></br>
-			<Link to="songs/"> #1052 The Oneness of Everything  Gathered Here</Link></div>
+			<Link to="songs/"> #1009 Meditation on Breathing</Link></div>
 			<div style={musicBorder}><strong>Offertory </strong> 
 			<br></br>
 			<div>piano<a href=""></a></div></div>
 			<div style={musicBorder}><strong>Closing Hymn </strong> 
 			<br></br>
-			<Link to="songs/">#325  Love Makes A Bridge</Link></div>
+			<Link to="songs/">#325 Love Makes A Bridge</Link></div>
+			<div style={musicBorder}><strong>Postlude </strong>
+			<br></br>
+			<>piano</></div>			
+		</div>
+		<div className='text-center m-5'>
+			<h2 style={{fontSize: 40}}><em>First Week of Advent</em></h2>
+			<h5 className='pt-2'><strong>Novemember 27, 2022</strong></h5>
+			<div style={musicBorder} ><strong>Prelude </strong>
+			<br></br>
+			<>piano</></div>
+			<div style={musicBorder}><strong>Chalice Song </strong>
+			<br></br>
+			<Link to="songs/" className='pt-2'>#1013 Open My Heart</Link></div>
+			<div style={musicBorder}><strong>Opening Hymn </strong> 
+			<br></br>
+			<Link to="songs/"> #1020 Woyaya</Link></div>
+			<div style={musicBorder}><strong>Centering Hymn </strong> 
+			<br></br>
+			<Link to="songs/"> #1008 When Our Heart Is in a Holy Place</Link></div>
+			<div style={musicBorder}><strong>Offertory </strong> 
+			<br></br>
+			<div>piano<a href=""></a></div></div>
+			<div style={musicBorder}><strong>Closing Hymn </strong> 
+			<br></br>
+			<Link to="songs/">#1028 The Fire of Commitment</Link></div>
 			<div style={musicBorder}><strong>Postlude </strong>
 			<br></br>
 			<>piano</></div>			
