154eaf33353c155fabc237944f2cda54c3b4f6bb
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2022-12-13 18:16:28 -0500

    .

--- src/components/UpcomingMusic.js
+++ src/components/UpcomingMusic.js
@@ -28,31 +28,6 @@ const UpcomingMusic = () => {
 			</li>
 		</div>
 		<hr></hr>
-		<div className='text-center'>
-			<h2 style={{fontSize: 40}}><em>Christmas Eve</em></h2>
-			<h5 className='pt-2' style={dateHeading}><strong>December 24, 2022</strong></h5>
-			<div style={musicBorder} ><strong>Prelude </strong>
-			<br></br>
-			<>piano</></div>
-			<div style={musicBorder}><strong>Chalice Song </strong>
-			<br></br>
-			<Link to="songs/" className='pt-2'>Joy Introit</Link></div>
-			<div style={musicBorder}><strong>Opening Hymn </strong> 
-			<br></br>
-			<Link to="songs/"> #298 Wake Now My Senses</Link></div>
-			<div style={musicBorder}><strong>Centering Hymn </strong> 
-			<br></br>
-			<Link to="songs/63786f9608d576294cc6d5de"> #388 Dona Nobis Pacem</Link></div>
-			<div style={musicBorder}><strong>Offertory </strong> 
-			<br></br>
-			<div>#18 What Wonderous Love<a href=""></a></div></div>
-			<div style={musicBorder}><strong>Closing Hymn </strong> 
-			<br></br>
-			<Link to="songs/">#16 Tis A Gift to Be Simple</Link></div>
-			<div style={musicBorder}><strong>Postlude </strong>
-			<br></br>
-			<>piano</></div>			
-		</div>
 		<div className='text-center m-5'>
 			<h2 style={{fontSize: 40}}><em>Guest Minister</em></h2>
 			<h5 className='pt-2' style={dateHeading}><strong>December 18, 2022</strong></h5>
@@ -61,23 +36,54 @@ const UpcomingMusic = () => {
 			<>piano</></div>
 			<div style={musicBorder}><strong>Chalice Song </strong>
 			<br></br>
-			<Link to="songs/" className='pt-2'>Joy Introit</Link></div>
+			<Link to="songs/6398f71d966ec532bd94ad37" className='pt-2'>Joy Introit</Link></div>
 			<div style={musicBorder}><strong>Opening Hymn </strong> 
 			<br></br>
-			<Link to="songs/">TBD</Link></div>
+			<Link to="songs/"># Love Will Guide Us</Link></div>
 			<div style={musicBorder}><strong>Centering Hymn </strong> 
 			<br></br>
-			<Link to="songs/63786f9608d576294cc6d5dd">TBD </Link></div>
+			<Link to="songs/6398f71d966ec532bd94ad44"># When Our Heart Is in a Holy Place </Link></div>
 			<div style={musicBorder}><strong>Offertory </strong> 
 			<br></br>
-			<div>piano<a href=""></a></div></div>
+			<div><a href="https://www.youtube.com/watch?v=X-_7XfaS7Mc">Loosen, Loosen</a></div></div>
 			<div style={musicBorder}><strong>Closing Hymn </strong> 
 			<br></br>
-			<Link to="songs/63786f9608d576294cc6d5e3">TBD</Link></div>
+			<a href="https://www.youtube.com/watch?v=a2vh87lWEuU">Every Time I Feel The Spirit</a></div>
 			<div style={musicBorder}><strong>Postlude </strong>
 			<br></br>
-			<>piano</></div>			
-			<hr></hr>
+			<div><a href="https://www.youtube.com/watch?v=88gtXTULVvY">Oh, River</a></div></div>			
+		<div className='text-center pt-5'>
+			<h2 style={{fontSize: 40}}><em>Christmas Eve</em></h2>
+			<h5 className='pt-2' style={dateHeading}><strong>December 24, 2022</strong></h5>
+			<div style={musicBorder} ><strong>Prelude </strong>
+			<br></br>
+			<>piano</></div>
+			<div style={musicBorder}><strong>Hymn </strong> 
+			<br></br>
+			<Link to="songs/"> # O Come All Ye Faithful</Link></div>
+			<div style={musicBorder}><strong>Solstice </strong> 
+			<br></br>
+			<Link to="songs/"> # Deck the Halls</Link></div>
+			<div style={musicBorder}><strong>Offertory </strong> 
+			<br></br>
+			<Link to='songs/6398f71d966ec532bd94ad3a'>Sing with Joy, Sing Noel!</Link></div>			
+			<div style={musicBorder}><strong>Kwanzaa </strong> 
+			<br></br>
+			<Link to="songs/"># Let There Be Peace on Earth</Link></div>
+			<div style={musicBorder}><strong>Christmas</strong> 
+			<br></br>
+			<Link to="songs/"># What Child is This</Link></div>
+			<div style={musicBorder}><strong>Hanukkah</strong> 
+			<br></br>
+			<Link to="songs/">#221 Light One Candle</Link></div>
+			<div style={musicBorder}><strong>UU</strong> 
+			<br></br>
+			<Link to="songs/"># We Are</Link></div>
+			<div style={musicBorder}><strong>UU</strong> 
+			<br></br>
+			<Link to="songs/">#251 Silent Night</Link></div>
+		</div>
+			{/* <hr></hr> */}
 		</div>
 		</>
 	)
