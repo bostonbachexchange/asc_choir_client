94ed566915ff704f5bccb778952b65b655d76da5
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2023-01-25 20:22:35 -0500

    .

--- src/components/UpcomingMusic.js
+++ src/components/UpcomingMusic.js
@@ -21,34 +21,34 @@ const UpcomingMusic = () => {
 		<div className='text-center'>
 			<h1>Music For Next rehearsal</h1>
 			<li style={rehearsalItems}>
-				<Link to='songs/63b71f9270bb4f80c8b7a41c'>Like a Mighty Stream</Link>
+				<Link to='songs/'>Like a Bridge Over Troubled Waters</Link>
 			</li>
 		</div>
 		<hr></hr>			
 		<div className='text-center pt-5'>
 			<h2 style={{fontSize: 40}}><em>Sunday Service</em></h2>
-			<h5 className='pt-2' style={dateHeading}><strong>January 15, 2023</strong></h5>
+			<h5 className='pt-2' style={dateHeading}><strong>January 29, 2023</strong></h5>
 			<div style={musicBorder} ><strong>Prelude: Jacob </strong>
 			<br></br>
-			<>Blue in Green</></div>
+			<>piano</></div>
 			<div style={musicBorder}><strong>Chalice Song: Choir</strong> 
 			<br></br>
 			<Link to="songs/63b71f9270bb4f80c8b7a41b">Flame Welcome All</Link></div>
 			<div style={musicBorder}><strong>Opening Hymn: Choir</strong> 
 			<br></br>
-			<Link to="songs/63b71f9270bb4f80c8b7a42b"> # 325 Love Makes a Bridge</Link></div>
+			<Link to="songs/"> # 346  Come Sing a Song With Me</Link></div>
 			<div style={musicBorder}><strong>Hymn: Choir</strong> 
 			<br></br>
-			<Link to="songs/63b71f9270bb4f80c8b7a409"> # 95 There Is More Love Somewhere</Link></div>
+			<Link to="songs/63b71f9270bb4f80c8b7a415"> # 1058  Be Ours a Religion</Link></div>
 			<div style={musicBorder}><strong>Offertory: Choir </strong> 
 			<br></br>
-			<Link to='songs/63b71f9270bb4f80c8b7a41c'>Like a Mighty Stream</Link></div>			
+			<Link to='songs/'># 1057  Go Lifted Up</Link></div>			
 			<div style={musicBorder}><strong>Closing Hymn: Choir</strong> 
 			<br></br>
-			<Link to="songs/63b71f9270bb4f80c8b7a40a"># 151 I Wish I Knew How</Link></div>
-			<div style={musicBorder}><strong>Postlude: Jacob</strong> 
+			<Link to="songs/63b71f9270bb4f80c8b7a41f"># 121  We’ll Build a Land</Link></div>
+			<div style={musicBorder}><strong>Postlude: Aarnav Tuladhar</strong> 
 			<br></br>
-			<>Con Alma</></div>
+			<>Maple Leaf Rag</></div>
 		</div>
 		</>
 	)
