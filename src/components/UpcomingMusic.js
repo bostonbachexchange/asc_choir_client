4e89fe06adcc75556825f5d587aabf2e092a2dbb
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2023-02-23 22:55:19 -0500

    .

--- src/components/UpcomingMusic.js
+++ src/components/UpcomingMusic.js
@@ -18,41 +18,58 @@ const UpcomingMusic = () => {
 
 	return (
 		<>
-		<div className='text-center'>
-			<h1>Music For Next rehearsal</h1>
-			<li style={rehearsalItems}>
-				<a href='https://www.youtube.com/watch?v=FcBdkOX1nnA'>The Gift of Love</a>
-			</li>
-			<li style={rehearsalItems}>
-				<Link to='songs/'>Like a Bridge Over Troubled Waters</Link>
-			</li>
-		</div>
-		<hr></hr>			
-		<div className='text-center pt-5'>
-			<h2 style={{fontSize: 40}}><em>Sunday Service</em></h2>
-			<h5 className='pt-2' style={dateHeading}><strong>February 9, 2023</strong></h5>
-			<div style={musicBorder} ><strong>Prelude: Jacob </strong>
-			<br></br>
-			<>The Man I Love, Gerswhin</></div>
-			<div style={musicBorder}><strong>Chalice Song: Choir</strong> 
-			<br></br>
-			<Link to="songs/">#12 May We Be Open to This Light</Link></div>
-			<div style={musicBorder}><strong>Opening Hymn: Choir</strong> 
-			<br></br>
-			<Link to="songs/"> #21 For the Beauty of the Earth</Link></div>
-			<div style={musicBorder}><strong>Centering Music: Beau</strong> 
-			<br></br>
-			<a href="https://vimeo.com/489421828"> Pipa Language || Lifted in Love by Lea Morris</a></div>
-			<div style={musicBorder}><strong>Offertory: Choir </strong> 
-			<br></br>
-			<Link to='songs/'>The Gift of Love, arr Hal H. Hopson</Link></div>			
-			<div style={musicBorder}><strong>Closing Hymn: Choir</strong> 
-			<br></br>
-			<Link to="songs/">#100 I'v got Peace Like a River</Link></div>
-			<div style={musicBorder}><strong>Postlude: Jacob</strong> 
-			<br></br>
-			<>Somebody Loves Me, Gershwin</></div>
-		</div>
+			{/* <div className='text-center'>
+				<h1>Music For Next rehearsal</h1>
+				<li style={rehearsalItems}>
+					<a href='https://www.youtube.com/watch?v=FcBdkOX1nnA'>The Gift of Love</a>
+				</li>
+				<li style={rehearsalItems}>
+					<Link to='songs/'>There is a Love</Link>
+				</li>
+			</div>
+			<hr></hr>			 */}
+			<div className='text-center pt-5'>
+				<h2 style={{fontSize: 40}}>
+					<em>Sunday Service</em>
+				</h2>
+				<h5 className='pt-2' style={dateHeading}>
+					<strong>February 26, 2023</strong>
+				</h5>
+				<div style={musicBorder} >
+					<strong>Prelude: Jacob</strong>
+					<br></br>
+					<>#149 Lift Every Voice and Sing</>
+				</div>
+				<div style={musicBorder}>
+					<strong>Chalice Song: Choir</strong> 
+					<br></br>
+					<Link to="songs/">#12 May We Be Open to This Light</Link>
+					</div>
+				<div style={musicBorder}>
+					<strong>Opening Hymn: Choir</strong> 
+					<br></br>
+					<Link to="songs/">#108 My Life Flows On</Link>
+				</div>
+				<div style={musicBorder}>
+					<strong>Centering Music: Choir</strong> 
+					<br></br>
+					<a href="https://www.uua.org/worship/words/music/there-love">There is a Love</a>
+				</div>
+				<div style={musicBorder}>
+					<strong>Offertory: Video </strong> 
+					<br></br>
+					<Link to='songs/'>Sweet Honey and the Rock</Link>
+				</div>			
+				<div style={musicBorder}>
+					<strong>Closing Hymn: Choir</strong> 
+					<br></br>
+					<Link to="songs/">#1018 Come and Go With Me </Link></div>
+				<div style={musicBorder}>
+					<strong>Postlude: Jacob</strong> 
+					<br></br>
+					<>Organ Grinder Blues</>
+				</div>
+			</div>
 		</>
 	)
 }
